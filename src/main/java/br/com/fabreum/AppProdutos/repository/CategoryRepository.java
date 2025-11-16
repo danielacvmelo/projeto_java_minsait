840732435138daa9808a2a1df8ec2626bc16cd58
danielacvmelo <danielacvmelo@gmail.com>
2025-11-16 17:26:22 -0300

    feat(dani-004): add full CRUD for Category

---
+++ src/main/java/br/com/fabreum/AppProdutos/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package br.com.fabreum.AppProdutos.repository;
+
+import br.com.fabreum.AppProdutos.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
