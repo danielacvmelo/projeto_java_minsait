cc22d13763e107b8040b35f7400e4defb3332281
danielacvmelo <danielacvmelo@gmail.com>
2025-11-17 12:43:13 -0300

    feat: Create CartItem entity

---
+++ src/main/java/br/com/fabreum/AppProdutos/model/CartItem.java
@@ -0,0 +1,32 @@
+package br.com.fabreum.AppProdutos.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "tb_cart_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+}
