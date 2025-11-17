af2580413e4b3b37b8bf2566fbdfa21b47836c76
danielacvmelo <danielacvmelo@gmail.com>
2025-11-17 12:42:15 -0300

    feat: Create Cart entity

---
+++ src/main/java/br/com/fabreum/AppProdutos/model/Cart.java
@@ -0,0 +1,46 @@
+package br.com.fabreum.AppProdutos.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_carts")
+@Getter
+@Setter
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> items = new HashSet<>();
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice = BigDecimal.ZERO;
+
+    public void addItem(CartItem item) {
+        items.add(item);
+        item.setCart(this);
+        recalculateTotalPrice();
+    }
+
+    public void removeItem(CartItem item) {
+        items.remove(item);
+        item.setCart(null);
+        recalculateTotalPrice();
+    }
+
+    public void recalculateTotalPrice() {
+        BigDecimal total = BigDecimal.ZERO;
+        for (CartItem item : items) {
+            total = total.add(item.getProduct().getPrice().multiply(new BigDecimal(item.getQuantity())));
+        }
+        this.totalPrice = total;
+    }
+}
