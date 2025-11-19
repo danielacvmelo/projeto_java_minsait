41510c057bfea062a481a45d2014412d907acf6e
barbarahellen <barbarahellen993@gmail.com>
2025-11-19 16:02:32 -0300

    feat: add review entity

---
+++ src/main/java/br/com/fabreum/AppProdutos/model/Review.java
@@ -0,0 +1,44 @@
+package br.com.fabreum.AppProdutos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;     // id do pedido relacionado
+
+    @Column(nullable = false)
+    private Integer rating;   // nota da review de 1 a 5
+
+    private String comment;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+}
